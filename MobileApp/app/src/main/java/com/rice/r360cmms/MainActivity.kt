56d8120eeeed090fc95b819ab03e6cd67b4b0f1e
Johann Pally <pallyjohann@gmail.com>
2021-09-15 15:58:08 -0500

    added onClick event for button

--- MobileApp/app/src/main/java/com/rice/r360cmms/MainActivity.kt
+++ MobileApp/app/src/main/java/com/rice/r360cmms/MainActivity.kt
@@ -2,11 +2,15 @@ package com.rice.r360cmms
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         //testing
+    }
+    fun sendMessage(view: View) {
+
     }
 }
