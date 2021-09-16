a24b13a9093e2898992f760774140875f2586591
MantejSingh21 <50661183+MantejSingh21@users.noreply.github.com>
2021-09-15 20:31:13 -0500

    Added a API call method using OkHttps3

--- MobileApp/app/src/main/java/com/rice/r360cmms/MainActivity.kt
+++ MobileApp/app/src/main/java/com/rice/r360cmms/MainActivity.kt
@@ -16,7 +16,18 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         //testing
     }
+
+    /**
+     * This function gets executed when the button is pressed.
+     * Inputs: View (not used)
+     * Outputs: NA
+     *
+     * This function executes an API call using okHtpps3 in the run function call.
+     * This function also executes a GET request using okHttps3 in the sendMessage function call.
+     */
     fun sendMessage(view: View) {
+
+        run("https://api.github.com/users/Evin1-/repos")
 
         val request = Request.Builder()
             .url("https://publicobject.com/helloworld.txt")
@@ -33,6 +44,22 @@ class MainActivity : AppCompatActivity() {
                     println(response.body().toString())
                 }
             }
+        })
+    }
+
+    /**
+     * This function executes a API call using OkHttps3
+     * Input: String - represents the URL to access
+     * Output: NA
+     */
+    private fun run(url: String) {
+        val request = Request.Builder()
+            .url(url)
+            .build()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {}
+            override fun onResponse(call: Call, response: Response) = println(response.body()?.string())
         })
     }
 
