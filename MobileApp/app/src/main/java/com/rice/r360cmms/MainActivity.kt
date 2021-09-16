701bd9c7d831be98bd48daa2ef4866a6f5e0af41
MantejSingh21 <50661183+MantejSingh21@users.noreply.github.com>
2021-09-15 20:23:06 -0500

    Added button that executes a GET request using OkHttps3

--- MobileApp/app/src/main/java/com/rice/r360cmms/MainActivity.kt
+++ MobileApp/app/src/main/java/com/rice/r360cmms/MainActivity.kt
@@ -3,8 +3,14 @@ package com.rice.r360cmms
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import okhttp3.*
+import java.io.IOException;
+
 
 class MainActivity : AppCompatActivity() {
+
+    private val client = OkHttpClient()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -12,5 +18,22 @@ class MainActivity : AppCompatActivity() {
     }
     fun sendMessage(view: View) {
 
+        val request = Request.Builder()
+            .url("https://publicobject.com/helloworld.txt")
+            .build()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                e.printStackTrace()
+            }
+            override fun onResponse(call: Call, response: Response) {
+                response.use {
+                    if (!response.isSuccessful) throw IOException("Unexpected code $response")
+                    println(response.headers())
+                    println(response.body().toString())
+                }
+            }
+        })
     }
+
 }
