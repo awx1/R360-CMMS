da36fc4af7881fa21d1432096972d15461431dcb
SAH4001 <Spencer.Huston5@gmail.com>
2021-09-20 10:32:47 -0500

    Add JSON to Gradle

--- Server/src/main/java/edu/rice/r360cmms/Server.java
+++ Server/src/main/java/edu/rice/r360cmms/Server.java
@@ -2,9 +2,12 @@ package edu.rice.r360cmms;
 
 import static spark.Spark.get;
 import static spark.Spark.staticFileLocation;
+import org.json.JSONObject;
 
 public class Server {
     private static final String TAG = "CMMSServer";
+    private static JSONObject database = new JSONObject();
+
 
     public static void main(String[] args) {
         staticFileLocation("/WebPublic");
