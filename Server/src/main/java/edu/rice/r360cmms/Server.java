8569ec6b2eae09aef8453ceb5b8f3f20921d535c
SAH4001 <Spencer.Huston5@gmail.com>
2021-09-20 15:18:55 -0500

    add comments and simple get push post delete

--- Server/src/main/java/edu/rice/r360cmms/Server.java
+++ Server/src/main/java/edu/rice/r360cmms/Server.java
@@ -1,8 +1,9 @@
 package edu.rice.r360cmms;
 
-import static spark.Spark.get;
+
 import static spark.Spark.staticFileLocation;
 import org.json.JSONObject;
+import spark.Spark;
 
 public class Server {
     private static final String TAG = "CMMSServer";
@@ -11,7 +12,25 @@ public class Server {
 
     public static void main(String[] args) {
         staticFileLocation("/WebPublic");
-        get(
+        Spark.get(//Returns JSON object
+            "/",
+            (request, response) -> {
+                //response.redirect("/week11images.html", 301); // you can find this file in /WebPublic
+                return database.toString();
+            });
+        Spark.put( //Replaces item in JSON object
+            "/",
+            (request, response) -> {
+                //response.redirect("/week11images.html", 301); // you can find this file in /WebPublic
+                return database.toString();
+            });
+        Spark.post( //adds new item to JSON object
+            "/",
+            (request, response) -> {
+                //response.redirect("/week11images.html", 301); // you can find this file in /WebPublic
+                return database.toString();
+            });
+        Spark.delete( //Deletes item
             "/",
             (request, response) -> {
                 //response.redirect("/week11images.html", 301); // you can find this file in /WebPublic
