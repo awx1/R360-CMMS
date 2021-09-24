3f31c0f570d199ee9f17590f2743914bff24cad2
Alex Xiong <awx1@rice.edu>
2021-09-24 15:36:52 -0500

    working server

--- Server/src/main/java/edu/rice/r360cmms/Server.java
+++ Server/src/main/java/edu/rice/r360cmms/Server.java
@@ -8,6 +8,7 @@ import spark.Spark;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.InputStream;
 
 public class Server {
@@ -17,12 +18,11 @@ public class Server {
 
     public static void main(String[] args) {
         File initialFile = new File("DB.json");
-        InputStream is = new FileInputStream(initialFile);
-        if (is == null) {
-            System.out.println("Crash");
-            throw new NullPointerException("Cannot find resource file " + initialFile.toString());
+        InputStream is = null;
+        try {
+            is = new FileInputStream(initialFile);
+        } catch (FileNotFoundException e) {
         }
-
         JSONTokener tokener = new JSONTokener(is);
         database = new JSONObject(tokener);
         staticFileLocation("/WebPublic");
@@ -76,13 +76,18 @@ public class Server {
                 //response.redirect("/week11images.html", 301); // you can find this file in /WebPublic
                 return ((JSONObject)database.get(request.params().get(":category"))).get(request.params().get(":object")).toString();
             });
-        Spark.put( //Replaces item in JSON object
-            "/",
+        Spark.put( //Replaces JSON object in a specific category
+            "/DB/:category/:object/:field/",
             (request, response) -> {
+                System.out.println(request.toString());
+                System.out.println(request.attributes().toString());
+                System.out.println(request.body().toString());
+                System.out.println(request.params().toString());
                 //response.redirect("/week11images.html", 301); // you can find this file in /WebPublic
-                return database.toString();
+                JSONObject newObject = new JSONObject(); // need to replace this with the object that gets passed in
+                return ((JSONObject)((JSONObject)database.get(request.params().get(":category"))).get(request.params().get(":object"))).put(request.params().get(":field"), newObject).toString();
             });
-        Spark.post( //adds new item to JSON object
+        Spark.post( //Adds a new JSON object to a specific category
             "/",
             (request, response) -> {
                 //response.redirect("/week11images.html", 301); // you can find this file in /WebPublic
